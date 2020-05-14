5db759a9d84e33eb55c20366435fa4b76da92671
Tolis Christomanos <christomanos@gmail.com>
2020-05-14 16:03:24 +0100

    Update site-games.js

--- site-games.js
+++ site-games.js
@@ -3,8 +3,8 @@ import http from "k6/http";
 
 export const options = {
   stages: [
-    { duration: "1m", target: 1000 },
-    { duration: "3m", target: 1000 },
+    { duration: "1m", target: 100 },
+    { duration: "3m", target: 100 },
     { duration: "1m", target: 0 }
   ]
 };
