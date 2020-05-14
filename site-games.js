ba6d65c182f6258285ddcb7c491caa5d17d9a06d
Tolis <tolis.christomanos@gamesys.co.uk>
2020-05-14 14:20:33 +0100

    adding site-games test

---
+++ site-games.js
@@ -0,0 +1,27 @@
+import { sleep } from "k6";
+import http from "k6/http";
+
+export const options = {
+  stages: [
+    { duration: "1m", target: 50 },
+    { duration: "3m", target: 50 },
+    { duration: "1m", target: 0 }
+  ]
+};
+
+export default function() {
+  let response;
+
+  response = http.get(
+    "http://35.193.240.177:3000/site-games/play-double-bubble",
+    {
+      headers: {
+        Environment: "release-next",
+        Venture: "virgingames",
+        "Accept-Language": "en-GB"
+      }
+    }
+  );
+
+  sleep(1);
+}
