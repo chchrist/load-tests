233fdbb3cd47be32757e0fd561f51a4c6a182f1d
Tolis Christomanos <christomanos@gmail.com>
2020-06-02 22:48:51 +0100

    Update layouts-slots.js

--- layouts-slots.js
+++ layouts-slots.js
@@ -4,12 +4,13 @@ import { Rate } from 'k6/metrics';
 const failureRate = new Rate('check_failure_rate');
 export const options = {
     stages: [
-        { target: 10, duration: '1m' },
-        { target: 10, duration: '3m' },
+        { target: 50, duration: '1m' },
+        { target: 50, duration: '3m' },
         { target: 0, duration: '1m' },
       ],
   thresholds: {
-    
+    'failed requests': ['rate<0.1'], // threshold on a custom metric
+    'http_req_duration': ['p(95)<500']  // threshold on a standard metric
   },
 };
 export default function() {
