747fe60da3b1c03c0eea9ccff7ef8176cea74626
Tolis Christomanos <christomanos@gmail.com>
2020-06-03 00:23:20 +0100

    Update layouts-slots.js

--- layouts-slots.js
+++ layouts-slots.js
@@ -4,9 +4,9 @@ import { Rate } from 'k6/metrics';
 const failureRate = new Rate('check_failure_rate');
 export const options = {
     stages: [
-        { target: 50, duration: '30s' },
-        { target: 50, duration: '1m' },
-        { target: 0, duration: '30s' },
+        { target: 1000, duration: '1m' },
+        { target: 1000, duration: '3m' },
+        { target: 0, duration: '1m' },
       ],
   thresholds: {
     'failed requests': ['rate<0.1'], // threshold on a custom metric
