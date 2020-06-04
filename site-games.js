8979a1a3e0ab8efe6ea0563e8705db737830675f
Tolis Christomanos <christomanos@gmail.com>
2020-06-04 23:23:58 +0100

    Update site-games.js

--- site-games.js
+++ site-games.js
@@ -1,7 +1,17 @@
 import http from 'k6/http';
-import { check, sleep } from 'k6';
-import { Rate } from 'k6/metrics';
-const failureRate = new Rate('check_failure_rate');
+import { sleep } from 'k6';
+
+let httpOptions = {
+  headers: {
+    Environment: 'release-next',
+    'Accept-Language': 'en-GB',
+    Venture: 'starspins',
+    Platform: 'desktop',
+    Partner: 'false',
+    Native: 'false',
+  },
+};
+
 export const options = {
   stages: [
     { target: 100, duration: '1m' },
@@ -16,17 +26,55 @@ export const options = {
     ],
   },
 };
-export default function() {
-  const response = http.get('http://10.128.0.26:3000/site-games/play-double-bubble', {
-    headers: {
-      Environment: 'release-next',
-      'Accept-Language': 'en-GB',
-      Venture: 'jackpotjoy',
-    },
-  });
-  const checkRes = check(response, {
-    'status is 200': r => r.status === 200,
-  });
-  failureRate.add(!checkRes);
-  sleep(1);
+
+export default function () {
+  let userDistro = Math.floor(Math.random() * 100);
+
+  switch (true) {
+    case userDistro <= 88:
+      http.batch([
+        ['GET', 'http://host.docker.internal:3000/categories', null, httpOptions],
+        ['GET', 'http://host.docker.internal:3000/layouts/homepage', null, httpOptions],
+      ]);
+
+      break;
+      case userDistro > 88 && userDistro <= 93:
+      http.batch([
+        ['GET', 'http://host.docker.internal:3000/categories', null, httpOptions],
+        ['GET', 'http://host.docker.internal:3000/layouts/slots', null, httpOptions],
+      ]);
+      break;
+    case userDistro > 93 && userDistro <= 96:
+      http.batch([
+        ['GET', 'http://host.docker.internal:3000/categories', null, httpOptions],
+        ['GET', 'http://host.docker.internal:3000/layouts/bingo', null, httpOptions],
+      ]);
+      break;
+    case userDistro > 96 && userDistro < 98:
+      http.batch([
+        ['GET', 'http://host.docker.internal:3000/categories', null, httpOptions],
+        ['GET', 'http://host.docker.internal:3000/layouts/free', null, httpOptions],
+      ]);
+      break;
+    case userDistro > 98 && userDistro < 99:
+      http.batch([
+        ['GET', 'http://host.docker.internal:3000/categories', null, httpOptions],
+        ['GET', 'http://host.docker.internal:3000/layouts/poker', null, httpOptions],
+        ['GET', 'http://host.docker.internal:3000/layouts/jackpots', null, httpOptions],
+      ]);
+      break;
+    case userDistro > 99 && userDistro < 100:
+      http.batch([
+        ['GET', 'http://host.docker.internal:3000/categories', null, httpOptions],
+        ['GET', 'http://host.docker.internal:3000/layouts/casino', null, httpOptions],
+        ['GET', 'http://host.docker.internal:3000/layouts/live-casino', null, httpOptions],
+      ]);
+      break;
+    default:
+      http.batch([
+        ['GET', 'http://host.docker.internal:3000/categories', null, httpOptions],
+        ['GET', 'http://host.docker.internal:3000/layouts/new', null, httpOptions],
+      ]);
+      break;
+  }
 }
