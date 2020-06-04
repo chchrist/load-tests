38337ceb60ad2f9d31acd97fa010619b9698de2c
Tolis Christomanos <christomanos@gmail.com>
2020-06-04 23:24:18 +0100

    Update layouts-slots.js

--- layouts-slots.js
+++ layouts-slots.js
@@ -1,32 +1,80 @@
 import http from 'k6/http';
-import { check, sleep } from 'k6';
-import { Rate } from 'k6/metrics';
-const failureRate = new Rate('check_failure_rate');
-export const options = {
-    stages: [
-        { target: 1000, duration: '1m' },
-        { target: 1000, duration: '3m' },
-        { target: 0, duration: '1m' },
-      ],
-  thresholds: {
-    'failed requests': ['rate<0.1'], // threshold on a custom metric
-    'http_req_duration': ['p(95)<200']  // threshold on a standard metric
+import { sleep } from 'k6';
+
+let httpOptions = {
+  headers: {
+    Environment: 'release-next',
+    'Accept-Language': 'en-GB',
+    Venture: 'starspins',
+    Platform: 'desktop',
+    Partner: 'false',
+    Native: 'false',
   },
 };
-export default function() {
-  const responseSlots = http.get('http://10.128.0.26:3000/layouts/slots', {
-    headers: {
-      Environment: 'release-next',
-      'Accept-Language': 'en-GB',
-      Platform: 'desktop',
-      Partner: false,
-      Venture: 'jackpotjoy',
-    },
-  });
-    
-//   const checkRes = check(response, {
-//     'status is 200': r => r.status === 200,
-//   });
-  //failureRate.add(!checkRes);
-  // sleep(1);
+
+export const options = {
+  stages: [
+    { target: 100, duration: '1m' },
+    { target: 100, duration: '3m' },
+    { target: 0, duration: '1m' },
+  ],
+  thresholds: {
+    http_req_duration: ['p(95)<100'],
+    check_failure_rate: [
+      'rate<0.01',
+      { threshold: 'rate<=0.1', abortOnFail: true },
+    ],
+  },
+};
+
+export default function () {
+  let userDistro = Math.floor(Math.random() * 100);
+
+  switch (true) {
+    case userDistro <= 88:
+      http.batch([
+        ['GET', 'http://host.docker.internal:3000/categories', null, httpOptions],
+        ['GET', 'http://host.docker.internal:3000/layouts/homepage', null, httpOptions],
+      ]);
+
+      break;
+      case userDistro > 88 && userDistro <= 93:
+      http.batch([
+        ['GET', 'http://host.docker.internal:3000/categories', null, httpOptions],
+        ['GET', 'http://host.docker.internal:3000/layouts/slots', null, httpOptions],
+      ]);
+      break;
+    case userDistro > 93 && userDistro <= 96:
+      http.batch([
+        ['GET', 'http://host.docker.internal:3000/categories', null, httpOptions],
+        ['GET', 'http://host.docker.internal:3000/layouts/bingo', null, httpOptions],
+      ]);
+      break;
+    case userDistro > 96 && userDistro < 98:
+      http.batch([
+        ['GET', 'http://host.docker.internal:3000/categories', null, httpOptions],
+        ['GET', 'http://host.docker.internal:3000/layouts/free', null, httpOptions],
+      ]);
+      break;
+    case userDistro > 98 && userDistro < 99:
+      http.batch([
+        ['GET', 'http://host.docker.internal:3000/categories', null, httpOptions],
+        ['GET', 'http://host.docker.internal:3000/layouts/poker', null, httpOptions],
+        ['GET', 'http://host.docker.internal:3000/layouts/jackpots', null, httpOptions],
+      ]);
+      break;
+    case userDistro > 99 && userDistro < 100:
+      http.batch([
+        ['GET', 'http://host.docker.internal:3000/categories', null, httpOptions],
+        ['GET', 'http://host.docker.internal:3000/layouts/casino', null, httpOptions],
+        ['GET', 'http://host.docker.internal:3000/layouts/live-casino', null, httpOptions],
+      ]);
+      break;
+    default:
+      http.batch([
+        ['GET', 'http://host.docker.internal:3000/categories', null, httpOptions],
+        ['GET', 'http://host.docker.internal:3000/layouts/new', null, httpOptions],
+      ]);
+      break;
+  }
 }
